611bb6fe67fbd0f614c866cbe76a47251d650d1a
jrivo <jrivo@DESKTOP-VL0RGIN>
2022-03-06 17:08:37 +0100

    "documentacion realizada"

--- src/BoardScreen.java
+++ src/BoardScreen.java
@@ -3,7 +3,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
-
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
